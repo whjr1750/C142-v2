0fa2859d66009c2837ece264137e8722b453f1eb
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2023-02-09 15:27:16 +0530

    added encoding='utf-8' in line 27

--- merge_csv.py
+++ merge_csv.py
@@ -24,6 +24,6 @@ for movie_item in all_movies:
             if movie_item[8] == movie_link_item[0]:
                 movie_item.append(movie_link_item[1])
                 if len(movie_item) == 28:
-                    with open("final.csv", "a+") as f:
+                    with open("final.csv", "a+",encoding='utf-8') as f:
                         csvwriter = csv.writer(f)
                         csvwriter.writerow(movie_item)
